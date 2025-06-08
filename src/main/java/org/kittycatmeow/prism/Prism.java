57a80a8bad412877d5c0e4f5d0db1c37e57e96bd
Alex Li <lialex764@gmail.com>
2025-06-07 22:39:53 -0400

    .

--- src/main/java/org/kittycatmeow/prism/Prism.java
+++ src/main/java/org/kittycatmeow/prism/Prism.java
@@ -18,7 +18,7 @@ public final class Prism extends JavaPlugin {
     private static FirstJoinDataHandler firstJoinDataHandler;
     private static PrismItemLibrary itemLibrary;
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Override
     public void onEnable() {
